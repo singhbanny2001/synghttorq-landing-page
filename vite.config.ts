8f5e02c9f72883865105af9dbb83827c4cc79532
Besil K E <besilke@Besils-MacBook-Air.local>
2026-06-15 12:22:36 +0530

    fix: enforce vercel preset in tanstack config

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,10 @@ export default async (env: any) => {
     tanstackStart: {
       // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
       // nitro/vite builds from this
-      server: { entry: "server" },
+      server: { 
+        entry: "server",
+        preset: "vercel"
+      },
     },
     vite: {
       plugins: [
